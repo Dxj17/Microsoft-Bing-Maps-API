a33a87b67526a8f619be6b577582acda195e8240
kevinkid <mac-bigkevin2682@gmail.com>
2017-01-02 13:33:33 +0300

    better changes

--- public/js/main.js
+++ public/js/main.js
@@ -4,11 +4,58 @@ Author: bigkevzs
 
 
 var map;
+var view = document.getElementsByClassName("details");
+
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('Map'), {
-        credentials: 'AuZTRcdNv_8_rFqQgk8uJfkPINqMSnpm44NcDbqolb-UrFaOOFPMpuUknYeA0crb'
+        credentials: 'AuZTRcdNv_8_rFqQgk8uJfkPINqMSnpm44NcDbqolb-UrFaOOFPMpuUknYeA0crb',
+        mapTypeId: Microsoft.Maps.MapTypeId.road,
+        zoom: 10
     });
 }
+
+
+function displayMapDetails(){
+var Detlist = document.createElement("ul");
+var domString = "\
+       <b>Map Center:</b><li>"+ map.getCenter() +"</li><br/>\
+       <b>Map Bounds: </b><li>"+ map.getBounds() +"</li><br/>\
+       <b> </b><li>"+ +"</li><br/>\
+       <b> </b><li>"+ +"</li><br/>\
+       <b> </b><li>"+ +"</li><br/>\
+    ";
+Detlist.innerHTML = domString;
+view.appendChild(Detlist,view.lastChild);
+
+}
+
+onload = displayMapDetails();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 onload = function(){
     // @todo: only use for testing purpose requesting location onload is suspecious for the user. 
@@ -18,5 +65,3 @@ onload = function(){
         });
     }
 }
-
-
