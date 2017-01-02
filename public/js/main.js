7f66d3ceb09c7121bcebff72d0674b98ff5a6bcd
kevinkid <mac-bigkevin2682@gmail.com>
2017-01-02 21:17:23 +0300

    debug using new services

--- public/js/main.js
+++ public/js/main.js
@@ -3,32 +3,47 @@ Author: bigkevzs
 */
 
 
-var map;
-var view = document.getElementsByClassName("details");
+var map,
+    view,
+    coord,
+    pushpin,
+    directionsManager;
+
+
+
+
+
 
 /**
  * @type {function} - display location data through dom injection
  */
 function displayMapDetails(){
-
+    
+    view = document.getElementsByClassName("details");
+    
     var Detlist = document.createElement("ul");
     
     var domString = "\
         <li><b>Map Center:</b> "+ map.getCenter() +"</li><br/>\
         <li><b>Map Bounds: </b> "+ map.getBounds() +"</li><br/>\
-        <li><b> </b> "+ +"</li><br/>\
-        <li><b> </b> "+ +"</li><br/>\
-        <li><b> </b> "+ +"</li><br/>\
         ";
+    
     Detlist.innerHTML = domString;
+
     view[0].appendChild(Detlist,view.lastChild);
 
 }
+
+
+
 
 /**
  * @type {function} - loads the map 
  */
 function loadMap() {
+    
+    directionsManager = document.getElementById("directionsManager");
+
 
 
     /**
@@ -40,9 +55,62 @@ function loadMap() {
         mapTypeId: Microsoft.Maps.MapTypeId.road,
         zoom: 15
     });
-    displayMapDetails();
+
+    displayMapDetails(); /// @todo: put at the bottom for consistency 
 
 
+
+    /// Setting Map Options 
+    map.setOptions({
+        center:map.getCenter(),
+        zoom:5,
+        maxZoom:15,
+        minZoom:5
+    });
+
+
+
+    /// @note: this service might require en enterprice key for geolocation accuracy which is valuable alot ! 
+    /// New Geolocator instance 
+    // var geolocator = new Microsoft.Maps.GeoLocationProvider(map);
+    
+    /// Get coordinates 
+    // coord = geolocator.getCurrentPosition();
+
+
+    /**
+     * @param{Object} - coordinates to the location 
+     * @param{Object} - pushpin settings 
+     */
+    pushpin = new Microsoft.Maps.Pushpin(map.getCenter() , {
+        icon: "http://localhost:3000/img/locationPushpin.png",
+        anchor: new Microsoft.Maps.Point(25, 25) /// @todo: replace prop value with the geolocation coords
+    });
+    
+
+
+
+
+    /**
+     * @param{Object} - Module namespace 
+     * @param{Function} - Callback function 
+     */
+    Microsoft.Maps.Map.loadModules('Microsoft.Maps.Directions', function(){
+
+        /// Instanciate the directionmanager 
+        var directionMan  = new Microsoft.Direction.DirectionsManager(map);
+        /// Set and Render the input directions controls 
+        directionMan.setRenderOptions({itineraryContainer: directionsManager});
+        directionMan.showInputPanel("directionsInputPanel");
+    });
+
+
+
+
+
+
+
+    map.entities.push(pushpin);
 
 
 }
